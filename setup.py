c20343979394d91a0b8efaf03838e9ea945ed383
Agustin Ona <agustin.ona@split.io>
2021-05-19 18:00:38 -0300

    mmmm

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='splitioToolbox',
-    version='0.0.1',
+    version='0.0.2',
     description='my set of tools for splitio python testing',
     url='git@github.com:agustinona/python-mytoolbox.git',
     author='Agustin Ona',
