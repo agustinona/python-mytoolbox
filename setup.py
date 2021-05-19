4f2c932d72a9c785b0077755dc42136579b19a55
Agustin Ona <agustin.ona@split.io>
2021-05-19 17:27:06 -0300

    basic setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='splitio-toolbox',
+    version='0.0.1',
+    description='my set of tools for splitio python testing',
+    url='git@github.com:agustinona/python-mytoolbox',
+    author='Agustin Ona',
+    author_email='agustinona@gmail.com',
+    license='unlicense',
+    packages=['splitio-toolbox'],
+    zip_safe=False
+)
