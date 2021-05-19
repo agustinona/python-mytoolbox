6385e0982c2e877ee191aea27ad08f45ceb1a5bc
Agustin Ona <agustin.ona@split.io>
2021-05-19 17:57:05 -0300

    created randomAlphanumKey tool

--- splitioToolbox/__init__.py
+++ splitioToolbox/__init__.py
@@ -1,2 +1,4 @@
-def saluteMe():
-    print("Hello there!")
+def randomAlphanumKey(len):
+    import random, string
+    x = ''.join(random.choices(string.ascii_letters + string.digits, k=len))
+    return x
